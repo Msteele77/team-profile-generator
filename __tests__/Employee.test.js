b826e616e6d0d4b13c5b67a0bec68cd5691620bd
Macheya Steele <mcheysteele@gmail.com>
2021-01-27 21:10:07 -0700

    set up all classes, started on tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@jest/globals');
+const Employee = require('../lib/Employee.js');
+
+test("Creates an employee object", () => {
+    const employee = new Emplyee 
+    expect(().toBe();
+
+});
