9001f01f505eede2d3db04faa40e857b963aa198
Macheya Steele <mcheysteele@gmail.com>
2021-02-01 16:39:33 -0700

    set up rest of prompts

--- Index.js
+++ Index.js
@@ -11,30 +11,127 @@ const initialPrompt = () => {
         {
         tpye: 'input',
         name: 'name',
-        message: "Enter your team member's name:",
+        message: "Enter your team managers's name:",
         },
-        {
-        type: 'list',
-        name: 'role',
-        message: "Select your team member's role.",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager"
-        ]},
         {
         type: 'input',
         name: 'id',
-        message: "Enter the team member's ID"
+        message: "Enter the team member's employee ID"
         },
         {
         type: 'input',
         name: 'email',
         message: "Enter the team member's email address."
+        },
+        {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Enter the team member's office number."
+        },
+        {
+        type: 'list',
+        name: 'role',
+        message: "Please choose the type of employee you want to add, or choose to finish builing your team.",
+        choices: ["Add an engineer", "Add an intern", "Complete team"
+            ]
         }
-    ]);
+    ])
 };
 
+const engineerOption = () => {
+    return inquirer.prompt([
+        {
+        tpye: 'input',
+        name: 'name',
+        message: "Enter the engineer's name:",
+        },
+        {
+        type: 'input',
+        name: 'id',
+        message: "Enter the eningeer's employee ID"
+        },
+        {
+        type: 'input',
+        name: 'email',
+        message: "Enter the engineer's email address."
+        },
+        {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Enter the engineer's Github Username."
+        },
+        {
+        type: 'list',
+        name: 'role',
+        message: "Would you like to add another employee or complete your team?",
+        choices: ["Add an engineer", "Add an intern", "Complete team"]
+        }
+    ])
+};
+
+const internOption = () => {
+    return inquirer.prompt([
+        {
+        tpye: 'input',
+        name: 'name',
+        message: "Enter the intern's name:",
+        },
+        {
+        type: 'input',
+        name: 'id',
+        message: "Enter the intern's employee ID"
+        },
+        {
+        type: 'input',
+        name: 'email',
+        message: "Enter the intern's email address."
+        },
+        {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Enter the name of the intern's school."        
+        },
+        {
+        type: 'list',
+        name: 'role',
+        message: "Would you like to add another employee or complete your team?",
+        choices: ["Add an engineer", "Add an intern", "Complete team"]
+        }
+    ])
+};
+
+const mainMenu = () => {
+    console.log(`
+    =========
+    MAIN MENU
+    =========
+    `);
+}
+
+function completeTeam () {
+    console.log(`
+    ========================================
+    Your team has been generated. Thank you!
+    ========================================`);
+    //FUNCTION WHERE THE HTML WILL BE GENERATED
+    
+}
 
 
-initialPrompt();
+
+initialPrompt()
+.then(data => {
+    switch (data.role) {
+        case "Add an engineer":
+            engineerOption();
+            break;
+        case "Add an intern":
+            internOption();
+            break;
+        case "Complete team":
+            completeTeam();
+            break;
+}})
+
+
+
