af7e99c0c42ed51d9d6a4a2935ce0a9345b1babb
Macheya Steele <mcheysteele@gmail.com>
2021-02-01 11:30:13 -0700

    completed engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
-const Engineer = require('../lib/Engineer.js');
+const Engineer = require("../lib/Engineer");
+
+test("set github account ", () => {
+    const testValue = "GitHubUser";
+    const engineer = new Engineer("john", 1, "test@test.com", testValue);
+    expect(engineer.github).toBe(testValue);
+  });
+  
+  test("getRole() should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const engineer = new Engineer("john", 1, "test@test.com", "GitHubUser");
+    expect(engineer.getRole()).toBe(testValue);
+  });
+  
+  test("get GitHub username from getGithub()", () => {
+    const testValue = "GitHubUser";
+    const engineer = new Engineer("john", 1, "test@test.com", testValue);
+    expect(engineer.getGithub()).toBe(testValue);
+  });
